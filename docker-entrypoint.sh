65c9148f02069beaa220c61b2b57f50c18009d7c
johnboisvert <john.boisvert@hotmail.com>
2026-01-20 13:36:36 -0500

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,5 @@
-#!/bin/sh
-set -e
+#!/usr/bin/env sh
+set -eu
 
-echo "ENTRYPOINT OK - PORT=$PORT"
-echo "CMD: $@"
-
-exec "$@"
+echo "PORT_ENV=${PORT:-<empty>}"
+exec python -m uvicorn main:app --host 0.0.0.0 --port "${PORT:-8000}" --log-level info
